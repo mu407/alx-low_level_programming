99bfce63efc5613e2a96e0084ac1024117e0a627
mu407 <67967096+mu407@users.noreply.github.com>
2023-10-23 15:44:04 +0400

    Update 4-free_listint.c

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -6,12 +6,12 @@
  */
 void free_listint(listint_t *head)
 {
-    listint_t *current;
+	listint_t *temp;
 
-    while (head)
-    {
-        current = head->next;
-        free(head);
-        head = current;
-    }
+	while (head)
+	{
+		temp = head->next;
+		free(head);
+		head = temp;
+	}
 }
