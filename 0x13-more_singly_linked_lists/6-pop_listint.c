37d3b674d86086e68d09c073699133d34b9ccc03
mu407 <67967096+mu407@users.noreply.github.com>
2023-10-23 15:39:37 +0400

    Update 6-pop_listint.c

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -12,17 +12,16 @@
  */
 int pop_listint(listint_t **head)
 {
-    listint_t *temp;
-    int data;
+	listint_t *temp;
+	int data;
 
-    if (!head || !*head)
-        return (0);
+	if (!head || !*head)
+		return (0);
 
-    data = (*head)->n;
-    temp = (*head)->next;
-    free(*head);
-    *head = temp;
+	data = (*head)->n;
+	temp = (*head)->next;
+	free(*head);
+	*head = temp;
 
-    return (data);
+	return (data);
 }
-
