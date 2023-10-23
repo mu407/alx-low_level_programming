1a74121b453f0192e5511d15a6f67e54c0429589
mu407 <67967096+mu407@users.noreply.github.com>
2023-10-23 15:26:26 +0400

    Update 10-delete_nodeint.c

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -1,43 +1,40 @@
 #include "lists.h"
 
 /**
- * delete_node_at_index - Deletes a node at a specific index in a linked list.
+ * delete_nodeint_at_index - Deletes a node at a specified index in a linked list.
  * @head: Pointer to the first element in the list.
- * @index: Index of the node to delete.
+ * @index: Index of the node to be deleted.
  *
- * Return: 1 (Success), or -1 (Failure).
+ * Return: 1 (Success), or -1 (Failure)
  */
-int delete_node_at_index(listint_t **head, unsigned int index)
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-    listint_t *current_node = *head;
-    listint_t *previous_node = NULL;
-    unsigned int i = 0;
+	listint_t *temp = *head;
+	listint_t *current = NULL;
+	unsigned int i = 0;
 
-    // Check if the list is empty.
-    if (*head == NULL)
-        return (-1);
+	if (*head == NULL)
+		return (-1);
 
-    // If the node to delete is the first node (index 0).
-    if (index == 0)
-    {
-        *head = (*head)->next;
-        free(current_node);
-        return (1);
-    }
+	if (index == 0)
+	{
+		*head = (*head)->next;
+		free(temp);
+		return (1);
+	}
 
-    // Traverse the list to find the node before the one to delete.
-    while (i < index - 1)
-    {
-        if (!current_node || !(current_node->next))
-            return (-1);
-        previous_node = current_node;
-        current_node = current_node->next;
-        i++;
-    }
+	while (i < index - 1)
+	{
+		if (!temp || !(temp->next))
+			return (-1);
+		temp = temp->next;
+		i++;
+	}
 
-    previous_node->next = current_node->next;
-    free(current_node);
 
-    return (1);
+	current = temp->next;
+	temp->next = current->next;
+	free(current);
+
+	return (1);
 }
-
